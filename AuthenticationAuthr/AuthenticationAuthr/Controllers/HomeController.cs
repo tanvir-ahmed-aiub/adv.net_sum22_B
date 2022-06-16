b83b86aa0224c78180758e95ece5b5e2d23fcaa7
Tanvir <tanvir.ahmeed.aiub@gmail.com>
2022-06-16 10:07:46 +0600

    Auth

---
+++ AuthenticationAuthr/AuthenticationAuthr/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using AuthenticationAuthr.Auth;
+using AuthenticationAuthr.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuthenticationAuthr.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Logged]
+        public ActionResult Index()
+        {
+            var db = new advsumm22_bEntities();
+            var id = Int32.Parse(Session["logged_user"].ToString());
+            var st = (from s in db.Students
+                      where s.Id == id
+                      select s).SingleOrDefault();
+            return View(st);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ AuthenticationAuthr/AuthenticationAuthr/Controllers/LoginController.cs
@@ -0,0 +1,39 @@
+﻿using AuthenticationAuthr.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuthenticationAuthr.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(Student model) {
+            var db = new advsumm22_bEntities();
+            var st = (from s in db.Students
+                      where s.Username.Equals(model.Username)
+                      && s.Password.Equals(model.Password)
+                      select s).SingleOrDefault();
+            if (st != null) {
+                Session["logged_user"] = st.Id;
+                return RedirectToAction("Index", "Home");
+            }
+            TempData["msg"] = "User Does not exist";
+            return View();
+
+        }
+        public ActionResult Logout() {
+            Session.Remove("logged_user");
+            return RedirectToAction("Index");
+            //Session.RemoveAll();
+        }
+    }
+}
